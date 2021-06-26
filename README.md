094c156406b84a8f19fb04e404ea3b0562eb054a
Marcos Leal <marcosleal@gmail.com>
2021-06-25 21:26:34 -0400

    wip: protecting discord keys

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # gameplay
-This is an app build with react-native and EXPO.
+This is an app build with react-native and expo.
 
+## background collor inside package.json
+      // "backgroundColor": "#070B24"
 
 
 ## How to run
 Download the code with git clone.
-On the command line type 
+On the command line type the command: 
 ```expo start```
 
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,13 +8,13 @@ import { ReactNode } from 'react';
 
 import * as AuthSession from 'expo-auth-session';
 
-import {
-  SCOPE,
-  CLIENT_ID,
-  CDN_IMAGE,
-  REDIRECT_URI,
-  RESPONSE_TYPE
-} from '../configs';
+
+const { SCOPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
+const { REDIRECT_URI } = process.env;
+const { RESPONSE_TYPE } = process.env;
+
 
 import { api } from '../services/api';
 
@@ -39,7 +39,8 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
   params: {
-    access_token: string;
+    access_token?: string;
+    error?: string;
   }
 }
 
@@ -58,7 +59,7 @@ function AuthProvider({ children }: AuthProviderProps) {
       const { type, params } = await AuthSession
         .startAsync({ authUrl }) as AuthorizationResponse;
 
-      if (type === 'success') {
+      if (type === 'success' && !params.error) {
         api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
         const userInfo = await api.get('/users/@me');
@@ -71,14 +72,12 @@ function AuthProvider({ children }: AuthProviderProps) {
           firstName,
           token: params.access_token
         });
-        setLoading(false);
-
-      } else {
-        setLoading(false);
       }
 
     } catch {
       throw new Error('Not possible to authenticate');
+    } finally {
+      setLoading(false);
     }
   }
 
